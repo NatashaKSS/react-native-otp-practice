76a241c79efcc83a9ab477632b9d6b8bb7c766e3
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-06-27 20:59:32 +0800

    Completed createUser function that saves user model to firebase

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,16 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);
+  // Verify the user provided a phone number
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad phone number input' });
+  }
+
+  // Format the phone number to remove non-digit symbols
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // Create a new user account using this phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(error => res.status(422).send({ error }));
 }
