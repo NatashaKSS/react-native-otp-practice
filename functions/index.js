300f5ca03f3d6639d2a8c66d421c3d26d9881c52
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-06-27 19:24:34 +0800

    Added initial skeleton of createUser function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const createUser = require('./create_user');
+const serviceAccount = require('../service-account-credentials.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://react-native-otp-practice.firebaseio.com"
 });
+
+exports.createUser = functions.https.onRequest(createUser);
